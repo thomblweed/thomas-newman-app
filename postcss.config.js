e1eee2bb700e68532c7c69488eb6b9649b5ce98e
Thomas Newman <thomblweed@gmail.com>
2022-08-24 17:06:51 +0100

    fix for css build error

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,15 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
-    tailwindcss("./tailwind.config.cjs"),
+    tailwindcss('./tailwind.config.cjs'),
     autoprefixer,
-    cssnano({
-      preset: "default",
-    }),
-  ],
+    process.env.NODE_ENV === 'production'
+      ? cssnano({
+          preset: 'default'
+        })
+      : undefined
+  ]
 };
