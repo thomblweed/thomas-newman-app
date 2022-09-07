e17fd954b98b3bd963146ef1cc3ad2a259bb8648
Thomas Newman <thomblweed@gmail.com>
2022-09-07 19:24:18 +0100

    flex body fix

--- app/layouts/Main/index.tsx
+++ app/layouts/Main/index.tsx
@@ -1,12 +1,14 @@
 import type { LinksFunction } from '@remix-run/node';
 import type { ReactNode } from 'react';
 
+import mainStyles from './main.css';
 import { Footer, links as footerStyles } from '~/components/Footer';
 import { Header } from '~/components/Header';
 import { links as headerStyles } from '~/components/Header/Desktop';
 import { DeviceProvider } from '~/state/provider/DeviceProvider';
 
 export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: mainStyles },
   ...headerStyles(),
   ...footerStyles()
 ];
@@ -17,9 +19,7 @@ export const Main = ({ children }: { children: ReactNode }) => (
     <DeviceProvider>
       <Header />
     </DeviceProvider>
-    <main className='container mx-auto w-11/12 max-w-screen-xl h-full'>
-      {children}
-    </main>
+    <main className='container main'>{children}</main>
     <Footer footerText='thomblweed' />
   </>
 );
