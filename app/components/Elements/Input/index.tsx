5144df33eab7fbf6dbe15454bf7ea89fa2e9d94c
Thomas Newman <thomblweed@gmail.com>
2022-10-05 19:34:51 +0100

    input element styles updates for mobile

--- app/components/Elements/Input/index.tsx
+++ app/components/Elements/Input/index.tsx
@@ -3,11 +3,6 @@ import type { InputHTMLAttributes } from 'react';
 
 import inputStyles from './input.css';
 
-const inputClasses =
-  'w-full rounded-md border-solid h-12 border-primary outline-none ' +
-  'p-4 border hover:shadow focus:shadow-fs focus:ring-primary focus:border-primary focus:ring-0 ' +
-  'transition-all duration-200 disabled:opacity-60 disabled:cursor-not-allowed bg-inherit';
-
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: inputStyles }
 ];
